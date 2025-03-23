6a9505c3163c43a65d8b5c86bdce6432f24856e8
felie2718 <francois-elie.ingwer@universite-paris-saclay.fr>
2025-03-23 20:19:21 +0100

    Update ActionPanel.java

--- src/View/ControlPanelComponents/Action/ActionPanel.java
+++ src/View/ControlPanelComponents/Action/ActionPanel.java
@@ -1,13 +1,41 @@
 package View.ControlPanelComponents.Action;
 
 import javax.swing.*;
-
 import Controller.Controller;
 
 public abstract class ActionPanel extends JPanel {
     protected static final int MARGIN = 5;
+    protected Controller controller;
 
+    // Default constructor
+    public ActionPanel() {
+        // Basic initialization of the panel
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        setBorder(BorderFactory.createEmptyBorder(MARGIN, MARGIN, MARGIN, MARGIN));
+    }
+
+    /**
+     * Updates the state of the action panel.
+     * This method must be implemented by derived classes.
+     */
     public abstract void update();
-    public abstract void connect(Controller c);
 
+    /**
+     * Connects the action panel to the controller.
+     * @param c The controller to connect.
+     */
+    public void connect(Controller c) {
+        this.controller = c;
+        // Additional logic to connect the panel to the controller
+    }
+
+    /**
+     * Resets the action panel.
+     * This method can be used to reset the state of the panel.
+     */
+    public void reset() {
+        removeAll();
+        revalidate();
+        repaint();
+    }
 }
