3f0c730b107a532b9bcefa3ffd0956f30045c788
felie2718 <francois-elie.ingwer@universite-paris-saclay.fr>
2025-03-23 20:19:53 +0100

    Update MarketPanel.java

--- src/View/ControlPanelComponents/MarketPanel.java
+++ src/View/ControlPanelComponents/MarketPanel.java
@@ -1,38 +1,63 @@
 package View.ControlPanelComponents;
 
 import View.CustomButton;
-
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MarketPanel extends JPanel {
 
-    //créer un constructeur
+    // Create a constructor
     public MarketPanel() {
-        //créer un layout pour afficher les boutons
+        // Create a layout to display the buttons
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-        //ajouter le mot "Market"
+        // Add the word "Market"
         JLabel marketLabel = new JLabel("Market");
         add(marketLabel);
 
-        //acheter un mouton
+        // Buy a sheep
         CustomButton buySheepButton = new CustomButton("Buy a sheep");
+        buySheepButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Action to perform when the "Buy a sheep" button is clicked
+                System.out.println("A sheep has been bought.");
+            }
+        });
         add(buySheepButton);
-        //TODO add listener
 
-        //acheter une brebis
+        // Buy an ewe
         CustomButton buyEweButton = new CustomButton("Buy an ewe");
+        buyEweButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Action to perform when the "Buy an ewe" button is clicked
+                System.out.println("An ewe has been bought.");
+            }
+        });
         add(buyEweButton);
-        //TODO add listener
 
-        //acheter une poule
+        // Buy a hen
         CustomButton buyHenButton = new CustomButton("Buy a hen");
+        buyHenButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Action to perform when the "Buy a hen" button is clicked
+                System.out.println("A hen has been bought.");
+            }
+        });
         add(buyHenButton);
-        //TODO add listener
 
-        //recruter un shepherd
+        // Hire a shepherd
         CustomButton hireShepherdButton = new CustomButton("Hire a shepherd");
+        hireShepherdButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Action to perform when the "Hire a shepherd" button is clicked
+                System.out.println("A shepherd has been hired.");
+            }
+        });
         add(hireShepherdButton);
-        //TODO add listener
     }
 }
