5f33adb4b18fb1445579045d7ee9ec073d61a9be
iz45h <izma.tahir@universite-paris-saclay.fr>
2025-03-31 15:41:28 +0200

    -Update Age mis a jour (plus d'erreur d'encapsulation)

--- src/Model/Predators/Predator.java
+++ src/Model/Predators/Predator.java
@@ -1,59 +1,55 @@
-//// Fichier : Model/Predators/Predator.java
-//package Model.Predators;
-//
-//import Model.Entity;
-//import Model.Exceptions.InvalidCoordinates;
-//import Model.Farm;
-//import Model.FarmAnimals.FarmAnimal;
-//import Model.Spot;
-//import java.awt.*;
-//
-//public abstract class Predator extends Entity {
-//    protected int aggressivity;
-//    protected int patience;   // Temps avant de retourner � son terrier
-//    protected int speed;      // La vitesse d�pend de l'agressivit�
-//    protected boolean fleeing = false;
-//
-//    public Predator(String name) {
-//        super(name);
-//    }
-//
-//    public Predator(Spot position, String name) {
-//        super(position, name);
-//    }
-//
-//    // Chaque pr�dateur vise une esp�ce sp�cifique d'�levage
-//    public abstract Class<? extends FarmAnimal> getTargetPreyClass();
-//
-//    // M�thode d'attaque sur une proie
-//    public void attack(FarmAnimal prey) {
-//        System.out.println(this.getName() + " attaque " + prey.getName());
-//        // Forcer l'�ge de la proie � 10 pour d�clencher la mort (via setAge)
-//        prey.setAge(10);
-//        System.out.println(prey.getName() + " a �t� tu� par " + this.getName());
-//    }
-//
-//
-//    @Override
-//    public void move(Point direction) throws InvalidCoordinates {
-//        if (this.getPosition() == null) {
-//            throw new InvalidCoordinates("Position non d�finie pour " + this.getName());
-//        }
-//        int newX = this.getPosition().getX() + direction.x;
-//        int newY = this.getPosition().getY() + direction.y;
-//        // R�cup�rer la r�f�rence au Farm depuis la position actuelle
-//        Farm currentFarm = this.getPosition().getFarm();
-//        // Cr�er un nouveau Spot avec les nouvelles coordonn�es et le farm
-//        this.setPosition(new Spot(newX, newY, currentFarm));
-//        System.out.println(this.getName() + " se d�place vers (" + newX + ", " + newY + ")");
-//    }
-//
-//
-//
-//    // M�thode de fuite lorsque le pr�dateur aper�oit un jardinier
-//    public void flee() {
-//        System.out.println(this.getName() + " aper�oit le jardinier et s'enfuit !");
-//        this.setPosition(null); // On peut consid�rer que le pr�dateur quitte la zone de jeu
-//        fleeing = true;
-//    }
+// Fichier : Model/Predators/Predator.java
+/*package Model.Predators;
+
+import Model.Entity;
+import Model.Exceptions.InvalidCoordinates;
+import Model.Farm;
+import Model.FarmAnimals.FarmAnimal;
+import Model.Spot;
+import java.awt.*;
+
+public abstract class Predator extends Entity {
+    protected int aggressivity;
+    protected int patience;   // Temps avant de retourner dans son terrier
+    protected int speed;      // La vitesse dépend de l'agressivité
+    protected boolean fleeing = false;
+
+    public Predator(String name) {
+        super(name);
+    }
+
+    public Predator(Spot position, String name) {
+        super(position, name);
+    }
+
+    // Chaque prédateur vise une espèce spécifique d'élevage
+    public abstract Class<? extends FarmAnimal> getTargetPreyClass();
+
+    // M�thode d'attaque sur une proie
+    public void attack(FarmAnimal prey) {
+        System.out.println(this.getId() + " attaque " + prey.getId());
+        // Forcer l'�ge de la proie � 10 pour d�clencher la mort (via setAge)
+        //prey.setAge(10);
+        System.out.println(prey.getId() + " a �t� tu� par " + this.getId());
+    }
+
+
+/*    @Override
+    public void move(Point direction) throws InvalidCoordinates {
+        if (this.getPosition() == null) {
+            throw new InvalidCoordinates("Position non d�finie pour " + this.getName());
+        }
+        int newX = this.getPosition().getX() + direction.x;
+        int newY = this.getPosition().getY() + direction.y;
+        // R�cup�rer la r�f�rence au Farm depuis la position actuelle
+        Farm currentFarm = this.getPosition().getFarm();
+        // Cr�er un nouveau Spot avec les nouvelles coordonn�es et le farm
+        this.setPosition(new Spot(newX, newY, currentFarm));
+        System.out.println(this.getName() + " se d�place vers (" + newX + ", " + newY + ")");
+    }
+*/
+
+
+    // Méthode de fuite lorsque le prédateur aperçoit un jardinier
+
 //}
--- src/Model/Predators/Wolf.java
+++ src/Model/Predators/Wolf.java
@@ -1,30 +1,36 @@
 //// Fichier : Model/Predators/Wolf.java
-//package Model.Predators;
-//
-//import Model.FarmAnimals.Sheep;
-//import Model.Exceptions.InvalidCoordinates;
-//import Model.Spot;
-//import java.awt.*;
-//
-//public class Wolf extends Predator {
-//
-//    public Wolf(String name) {
-//        super(name);
-//        this.aggressivity = 5; // Valeur d'exemple
-//        this.patience = 10;
-//        this.speed = aggressivity * 2;
-//    }
-//
-//    public Wolf(Spot position, String name) {
-//        super(position, name);
-//        this.aggressivity = 5;
-//        this.patience = 10;
-//        this.speed = aggressivity * 2;
-//    }
-//
-//    @Override
-//    public Class getTargetPreyClass() {
-//        // Les loups chassent les brebis/moutons
-//        return Sheep.class;
-//    }
-//}
+/*package Model.Predators;
+
+import Model.FarmAnimals.Sheep;
+import Model.Exceptions.InvalidCoordinates;
+import Model.Spot;
+import java.awt.*;
+
+public class Wolf extends Predator {
+
+    public Wolf(String name) {
+        super(name);
+        this.aggressivity = 5; // Valeur d'exemple
+        this.patience = 10;
+        this.speed = aggressivity * 2;
+    }
+
+    public Wolf(Spot position, String name) {
+        super(position, name);
+        this.aggressivity = 5;
+        this.patience = 10;
+        this.speed = aggressivity * 2;
+    }
+
+    @Override
+    public Class getTargetPreyClass() {
+        // Les loups chassent les brebis/moutons
+        return Sheep.class;
+    }
+
+    @Override
+    public String getSpecies() {
+        return null;
+    }
+}
+*/
